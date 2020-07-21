386d01065d61df6317b509c148a17de03a08a1d0
charleschrist21 <charleschrist21@gmail.com>
2020-07-21 22:31:38 +0700

    world 1

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:new3/pages/choose_location.dart';
+import 'package:new3/pages/home.dart';
+import 'package:new3/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
